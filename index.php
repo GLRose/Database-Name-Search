2936cc910446c32a571940404b12b2cb6517b303
Garrett Rose <garrett.rose03@gmail.com>
2018-02-21 11:56:11 -0500

    used include

--- index.php
+++ index.php
@@ -10,32 +10,7 @@
         </form>
     </div>
     <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "searchapp";
-
-        if(isset($_POST['submit'])){
-        $search_value=$_POST["search"];
-        $conn=new mysqli($servername,$username,$password,$dbname);
-        if($conn->connect_error){
-            echo 'Connection Failed: '.$con->connect_error;
-            }else{
-                $sql="select * from people where firstname like '%$search_value%' or lastname like '%$search_value%'";
-    
-                $res=$conn->query($sql);
-
-                while($row=$res->fetch_assoc()){
-                    echo $row["firstname"];
-                
-                    echo $row["lastname"];
-                    echo "</br>";
-                
-
-                    }       
-
-                }
-            }
-?>
+    include_once('conn.php');
+    ?>
 </body>
 </html>
