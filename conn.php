dcb90d190367c133698ce975a387b31d2ce48391
Garrett Rose <garrett.rose03@gmail.com>
2018-02-21 11:57:18 -0500

    include

--- conn.php
+++ conn.php
@@ -1,23 +1,28 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "searchapp";
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "searchapp";
 
-$search_value=$_POST["search"];
-$conn=new mysqli($servername,$username,$password,$dbname);
-if($conn->connect_error){
-    echo 'Connection Failed: '.$con->connect_error;
-    }else{
-        $sql="select * from people where firstname like '%$search_value%'";
+        if(isset($_POST['submit'])){
+        $search_value=$_POST["search"];
+        $conn=new mysqli($servername,$username,$password,$dbname);
+        if($conn->connect_error){
+            echo 'Connection Failed: '.$con->connect_error;
+            }else{
+                $sql="select * from people where firstname like '%$search_value%' or lastname like '%$search_value%'";
+    
+                $res=$conn->query($sql);
 
-        $res=$con->query($sql);
+                while($row=$res->fetch_assoc()){
+                    echo $row["firstname"];
+                
+                    echo $row["lastname"];
+                    echo "</br>";
+                
 
-        while($row=$res->fetch_assoc()){
-            echo 'Firstname:  '.$row["firstname"];
+                    }       
 
-
-            }       
-
-        }
+                }
+            }
 ?>
